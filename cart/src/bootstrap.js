13d16644abc4f453a54c59c2a27efe2dabfc2d1a
Antonio <a.mancarella@presstoday.com>
2023-05-12 17:34:13 +0200

    refactor bootstrap.js file in cart folder

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -1,5 +1,15 @@
-import faker from 'faker'
+import faker from 'faker';
 
-const cartText = `<div>You have ${faker.random.number()} in your cart</div`
+const mount = (el) => {
+    const cartText = `<div>You have ${faker.random.number()} in your cart</div`
+    document.querySelector('#dev-cart').innerHTML = cartText
+}
 
-document.querySelector('#dev-cart').innerHTML = cartText
+if(process.env.NODE_ENV === 'development'){
+    const el = document.querySelector("#dev-cart")
+    if(el){
+        mount(el);
+    }
+}
+
+export { mount };
