5d7244671e40d0db196ab32e7097031edf0c4023
Antonio <a.mancarella@presstoday.com>
2023-05-12 17:25:42 +0200

    refactor bootstrap.js file in container folder to import mount function from products mf

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,5 @@
-import 'products/ProductsIndex';
+import { mount } from 'products/ProductsIndex';
 import 'cart/CartShow';
 
+mount(document.querySelector('#my-products'));
 console.log('Container!');
