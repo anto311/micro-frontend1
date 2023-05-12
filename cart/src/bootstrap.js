483948af52ead222274a62394532cafef5764330
Antonio <a.mancarella@presstoday.com>
2023-05-12 18:01:43 +0200

    refactor bootstrap.js file in container folder to import mount function from cart mf and bootstrap.js from cart folder refactor

--- cart/src/bootstrap.js
+++ cart/src/bootstrap.js
@@ -2,7 +2,7 @@ import faker from 'faker';
 
 const mount = (el) => {
     const cartText = `<div>You have ${faker.random.number()} in your cart</div`
-    document.querySelector('#dev-cart').innerHTML = cartText
+    el.innerHTML = cartText
 }
 
 if(process.env.NODE_ENV === 'development'){
